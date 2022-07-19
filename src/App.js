7412d02c3f91b4483445b2f9ecfca68cbb878c2b
Jonathan Shnayder (Intern) <jshnayder@lm-c02fm3vgq05n.fios-router.home>
2022-07-19 15:14:55 -0400

    Added button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import db from './firebase'
-import { doc, collection, setDoc, serverTimestamp} from 'firebase/firestore'
+import { doc, collection, setDoc, serverTimestamp } from 'firebase/firestore'
+import { Button, TextField } from '@mui/material';
 
 function App() {
+  const [value, setValue] = useState('')
+  
+  const handleChange = event => {
+    setValue(event.target.value)
+  }
   const createPost = async () => {
     console.log('Creating POst')
     const newPostRef = doc(collection(db, "posts"));
-        await setDoc(newPostRef, {
-            content: 'This is a test post from react',
-            timestamp: serverTimestamp()
-        });
+    await setDoc(newPostRef, {
+        content: value,
+        timestamp: serverTimestamp()
+    });
   }
   
   return (
@@ -29,9 +35,12 @@ function App() {
         >
           Learn React
         </a>
-        <button onClick={createPost}>Create Post</button>
+        <Button variant="contained" onClick={createPost}>Create Post</Button>
+    
+        <TextField style={{color: "white"}} value={value} onChange={handleChange} variant="outlined" />
       </header>
     </div>
+
   );
 }
 
